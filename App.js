aa9ac1797b25e66ebf5317e30432e37e7238c4a8
jvsobraz <jvsobraz@hotmail.com>
2023-04-08 17:07:40 -0300

    Adicionando os imports no App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { Button, Text, View } from "react-native";
+import auth from '@react-native-firebase/auth';
+
+const App = (props) => {
+
+
+}
+
+export default App;
