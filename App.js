c0b7b6ade9a1a42bef42f4c4b56170bec5602e4b
jvsobraz <jvsobraz@hotmail.com>
2023-04-08 17:16:47 -0300

    adicionando dentro do componente App as seguintes lógicas:

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import React, { useState, useEffect } from "react";
-import { Button, Text, View } from "react-native";
+import React, { 
+    useState, 
+    useEffect 
+} from "react";
+
+import { 
+    Button,
+     Text,
+      View 
+    } from "react-native";
+
 import auth from '@react-native-firebase/auth';
 
 const App = (props) => {
 
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
 
-}
+    if (initializing) {
+        return null;
+    }
+
+    if (!user) {
+        return (
+            <View>
+                <Button
+                    onPress={ () => auth().signInAnonymously().then((user) => setUser(user)) }
+                    title="Acessar" />
+            </View>
+        );
+    }
+
+    return (
+        <View>
+            <Text>Olá usuário!</Text>
+
+            <Button
+                onPress={() => auth.().signOut() }
+                titl="Sair" />
+        </View>
+    );
+ }
 
 export default App;
